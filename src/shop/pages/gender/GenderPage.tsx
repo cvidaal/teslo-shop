0c3d13e92addd31792740e7d0b81cedcef8022b8
Carlos Vidal <cvidals.dev@gmail.com>
2025-12-12 11:13:06 +0100

    Custom Logo, routa con parametros de genero y login, register page

--- src/shop/pages/gender/GenderPage.tsx
+++ src/shop/pages/gender/GenderPage.tsx
@@ -1,3 +1,20 @@
+import { CustomPagination } from "@/components/custom/CustomPagination";
+import { products } from "@/data/products.data";
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron";
+import { ProductsGrid } from "@/shop/components/ProductsGrid";
+import { useParams } from "react-router";
+
 export const GenderPage = () => {
-  return <div>GenderPage</div>;
+  const { gender } = useParams();
+
+  const genderLabel =
+    gender === "men" ? "Hombre" : gender === "women" ? "Mujeres" : "Niños";
+
+  return (
+    <>
+      <CustomJumbotron title={`Productos para ${genderLabel}`} />
+      <ProductsGrid products={products} />
+      <CustomPagination totalPages={7} />
+    </>
+  );
 };
